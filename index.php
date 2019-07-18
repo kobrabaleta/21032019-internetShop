aa994bc318b185652cf51c406d49abcf5a955b2d
Kseniya Udod <kobrabaleta@gmail.com>
2019-07-18 20:52:04 +0300

    Приветики index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  $title = 'главная страница';
  include $_SERVER['DOCUMENT_ROOT'] . '/modules/head.php';
  include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php';
-
+// Приветище!
 ?>
 
     <body>
